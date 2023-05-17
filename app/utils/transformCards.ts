001f535cfa6af62f2b93c44fd592a886d0172e33
Kevin <kevin@beacons.ai>
2023-05-17 09:31:54 -0700

    transform

---
+++ app/utils/transformCards.ts
@@ -0,0 +1,10 @@
+import { ICard } from '../Types/ICard';
+import { ITransformedCard } from '../Types/ITransformedCard';
+
+export default function transformCards(cards: ICard[]): ITransformedCard[] {
+  return cards.map((card) => {
+    return {
+      imagesrc: card.imagesrc,
+    };
+  });
+}
