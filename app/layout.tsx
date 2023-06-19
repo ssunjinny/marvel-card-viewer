6781a5b0619ede1e829ef09c23b55822473507a7
Kevin <kevin@beacons.ai>
2023-06-19 14:27:44 -0700

    footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Outfit } from 'next/font/google';
 
 import TextHeading from './components/Typography/TextHeading';
 import CardCodeSelect from './components/CardCodeSelect';
+import Link from 'next/link';
 
 export const metadata = {
   title: 'Marvel Card Viewer',
@@ -34,6 +35,13 @@ export default function RootLayout({ children }: IRootLayoutProps) {
         <main className="flex min-h-screen flex-col items-center justify-between pt-6 lg:pt-12 px-6">
           {children}
         </main>
+        <footer>
+          <div className="mt-6 lg:mt-12 py-2 bg-primary text-white text-center">
+            <Link href="https://github.com/ssunjinny">
+              created by ssunjinny
+            </Link>
+          </div>
+        </footer>
       </body>
     </html>
   );
